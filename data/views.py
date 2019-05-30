ccad09bc8dcefe446661af5f3bc9794325c557d2
shubhrant <shubhrant999@gmail.com>
2019-05-30 16:59:57 +0530

    Post form data

--- data/views.py
+++ data/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
 
 # Create your views here.
 def homePage(requests):
@@ -10,12 +11,15 @@ def homePage(requests):
 def aboutPage(requests):
     return render(requests, 'about.html', {})
 
-# Create your views here.
-def aboutPage1(requests):
-    return render(requests, 'about.html', {})
+def searchPage(requests):
+    if requests.method == 'POST':
+        data = {
+            'type': requests.POST['type'],
+        } 
 
-
-
+        return render(requests, 'property-grid.html', {'listp': data})
+    else:
+        return render(requests, 'about.html', {})
 
 
 
