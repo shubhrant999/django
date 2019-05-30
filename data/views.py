b915822ff26bcc572354d44e74dc4d61a0b9c04e
shubhrant <shubhrant999@gmail.com>
2019-05-30 19:10:10 +0530

    Todays final

--- data/views.py
+++ data/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
+from .forms import ProductForm
 
 # Create your views here.
 def homePage(requests):
@@ -12,12 +13,21 @@ def aboutPage(requests):
     return render(requests, 'about.html', {})
 
 def searchPage(requests):
+    productform = ProductForm()
+
     if requests.method == 'POST':
         data = {
+            'keyword': requests.POST['keyword'],
             'type': requests.POST['type'],
-        } 
+            'city': requests.POST['city'],
+            'bedroom': requests.POST['bedroom'],
+            'garage': requests.POST['garage'],
+            'bathroom': requests.POST['bathroom'],
+            'price': requests.POST['price'],
+        }
 
-        return render(requests, 'property-grid.html', {'listp': data})
+
+        return render(requests, 'property-grid.html', {'form': productform, 'data':data})       
     else:
         return render(requests, 'about.html', {})
 
